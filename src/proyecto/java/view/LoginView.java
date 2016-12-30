3c9facc5312f017e49152653932ad56060552ee2
KimberlyEs <kesquivel37@gmail.com>
2016-12-30 14:09:33 -0600

    Cambiar cosas de LoginView

--- src/proyecto/java/view/LoginView.java
+++ src/proyecto/java/view/LoginView.java
@@ -26,7 +26,7 @@ public class LoginView extends JFrame {
 
     JTextField txtUsername = new JTextField(15);
     JPasswordField txtPassword = new JPasswordField(15);
-    JButton btnLogin = new JButton("Iniciar sesion");
+    JButton btnLogin = new JButton("Iniciar Sesión");
     JButton btnSalir = new JButton("Salir");
 
     /**
@@ -35,23 +35,23 @@ public class LoginView extends JFrame {
      */
     public LoginView() throws IOException {
         super("Inicio de Sesion");
-        
+
         setSize(300, 200);
-        
+        setLayout(new BorderLayout());
+
+        //create controller
         LoginController logController = new LoginController(txtUsername, txtPassword, btnLogin, btnSalir);
         btnLogin.addActionListener(logController);
         btnSalir.addActionListener(logController);
-        
-       // setLookAndFeel();
-        setLayout(new BorderLayout());
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        //create panel and change color
         JPanel panel = new JPanel();
         panel.setBackground(Color.WHITE);
 
         JLabel usernameLabel = new JLabel("Username: ");
         JLabel passwordLabel = new JLabel("Password: ");
 
-        //sets
+        //sets text 
         txtUsername.setName("username");
         txtPassword.setName("password");
         btnLogin.setName("Login");
@@ -59,8 +59,10 @@ public class LoginView extends JFrame {
         usernameLabel.setName("lblUsername");
         passwordLabel.setName("lblPassword");
 
-       
+        btnLogin.setBackground(Color.cyan);
+        btnSalir.setBackground(Color.cyan);
 
+        //add components
         panel.setName("Form");
         panel.add(usernameLabel);
         panel.add(txtUsername);
@@ -68,14 +70,14 @@ public class LoginView extends JFrame {
         panel.add(txtPassword);
         panel.add(btnLogin);
         panel.add(btnSalir);
-        
 
         add(panel);
-
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //pack();
+        setLocationRelativeTo(null);
         setVisible(true);
 
     }
-
 
     private static void setLookAndFeel() {
         try {
@@ -86,7 +88,7 @@ public class LoginView extends JFrame {
             // ignore error
         }
     }
-    
+
     /**
      *
      * @param args
