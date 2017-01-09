f285a4f7c6cb269632695e91fe073c5ba16f9b1d
Yeni <Yeni@EsquivelReyes>
2017-01-09 11:16:29 -0600

    Arreglo del test AddOfficeJUnitTest

--- test/AddOfficeJUnitTest.java
+++ test/AddOfficeJUnitTest.java
@@ -4,12 +4,14 @@
  * and open the template in the editor.
  */
 
+import static java.awt.Frame.MAXIMIZED_BOTH;
 import java.io.IOException;
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
 import org.assertj.swing.fixture.JPanelFixture;
 import org.junit.After;
 import static org.junit.Assert.assertArrayEquals;
+import org.junit.Before;
 import org.junit.Test;
 import proyecto.java.view.AddOfficeView;
 import proyecto.java.view.PrincipalPanelView;
@@ -25,17 +27,23 @@ public class AddOfficeJUnitTest {
     private JPanelFixture panel;
 
     public AddOfficeJUnitTest() {
+   
+    }
+    
+      @Before
+    public void setUp() throws IOException {
         PrincipalPanelView frame = GuiActionRunner.execute(() -> new PrincipalPanelView());
         window = new FrameFixture(frame);
+        frame.setVisible(true); 
         window.show();
+        frame.setExtendedState(MAXIMIZED_BOTH);
+        AddOfficeView ventanaInterna = new AddOfficeView();
+        window.menuItem("agregarConsultorio").click();
+        ventanaInterna.setVisible(true);
     }
 
     @Test
     public void testVisibleComponents() throws IOException {
-        AddOfficeView ventanaInterna = new AddOfficeView();
-        window.menuItem("agregarConsultorio").click();
-        ventanaInterna.setVisible(true);
-
         window.label("lblName").requireVisible();
         window.label("lblPhone").requireVisible();
         window.label("lblDays").requireVisible();
@@ -46,27 +54,20 @@ public class AddOfficeJUnitTest {
 
     @Test
     public void contentGendersComboBox() throws IOException {
-        AddOfficeView ventanaInterna = new AddOfficeView();
-        window.menuItem("agregarConsultorio").click();
-        ventanaInterna.setVisible(true);
-
         String[] offices = {"Hospital", "Clinica"};
         assertArrayEquals(window.comboBox("offices").contents(), offices);
     }
 
     @Test
     public void isCorrectInformation() throws IOException {
-        AddOfficeView ventanaInterna = new AddOfficeView();
-        window.menuItem("agregarConsultorio").click();
-        ventanaInterna.setVisible(true);
-
         window.textBox("name").enterText("Clinica Tres Rios");
         window.textBox("phone").enterText("22796352");
         window.textBox("days").enterText("Miercoles");
-        window.textBox("hourStart").enterText("8:00 AM");
-        window.textBox("hourEnd").enterText("6:30 PM");
+        window.textBox("hourStart").enterText("800 AM");
+        window.textBox("hourEnd").enterText("630 PM");
         window.comboBox("offices").selectItem("Clinica");
         window.button("ok").click();
+        
     }
 
     @After
