954ce954640fa5d327416dd470261d85f094ecfe
YeniferEsqu <gemeyeni@hotmail.com>
2017-01-09 20:38:19 -0600

    Vista de verificarCitas del paciente

--- src/proyecto/java/view/AppointmentPatientView.java
+++ src/proyecto/java/view/AppointmentPatientView.java
@@ -5,10 +5,36 @@
  */
 package proyecto.java.view;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import java.io.IOException;
+import javax.swing.JButton;
+import javax.swing.JInternalFrame;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import proyecto.java.controller.AppointmentPatientController;
+
 /**
  *
  * @author Yeni
  */
-public class AppointmentPatientView {
+public class AppointmentPatientView extends JInternalFrame {
+    JButton verify= new JButton("Buscar Citas");
+    JTable table = new JTable();
+    DefaultTableModel tableModel;
+    
+    public AppointmentPatientView() throws JsonGenerationException,
+            JsonMappingException, IOException {
+
+        super("Lista de Citas", true, true, true, true);
+        verify.setName("btnFilter");
+
+        // Create table model
+        table.setName("mainTable");
+        table.setModel(tableModel);
+        
+        AppointmentPatientController controller = new AppointmentPatientController(verify,tableModel);
+        verify.addActionListener(controller);
+    }
     
 }
